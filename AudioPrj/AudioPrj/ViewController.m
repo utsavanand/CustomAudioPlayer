fe79bfcddbcb1f4076f33afd395c971883aed16f
utsavanand <KulizaMacBookPro@utsavanand-2.local>
2013-12-13 17:51:28 +0530

    Further UI Changes

--- AudioPrj/AudioPrj/ViewController.m
+++ AudioPrj/AudioPrj/ViewController.m
@@ -82,11 +82,6 @@
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (allowFrameChange) {
-
-    
-    NSLog(@"UPDATE HEY-%@",indexPath);
-    }
     static NSString *tableCellIdentifier = @"AudioCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableCellIdentifier];
     if (cell == nil) {
@@ -103,17 +98,32 @@
     [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
 
     // To adjust the imcomplete separator in iOS7 tableview
-    
     if ([tableView respondsToSelector:@selector(setSeparatorInset:)]) {
         [tableView setSeparatorInset:UIEdgeInsetsZero];
     }
-    
     UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(15, 10, 200, 40)];
     lbl.text=[tableData objectAtIndex:indexPath.row];
     lbl.backgroundColor = [UIColor clearColor];
     [cell.contentView addSubview:lbl];
-    if (indexPath.row==activeRow && allowFrameChange) {
-    [aSlider setFrame:CGRectMake(50, lbl.frame.size.height+lbl.frame.origin.y, 200, 40)];
+    
+    if (indexPath.row==activeRow && allowFrameChange){
+    
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        button.tag=indexPath.section;
+        if (audioPlayer.playing) {
+            [button setTitle:@"Pause" forState:UIControlStateNormal];
+        }
+        else{
+        [button setTitle:@"Play" forState:UIControlStateNormal];
+        }
+        [button addTarget:self
+                   action:@selector(audioShuffle:)
+         forControlEvents:UIControlEventTouchDown];
+        button.frame = CGRectMake(10, lbl.frame.size.height+lbl.frame.origin.y, 60, 40);
+        [cell.contentView addSubview:button];
+        
+    [aSlider setFrame:CGRectMake(80, lbl.frame.size.height+lbl.frame.origin.y, 200, 40)];
     [aSlider addTarget:self action:@selector(sliderChanged:)
       forControlEvents:(UIControlEventTouchUpInside | UIControlEventTouchUpOutside)];
     aSlider.backgroundColor = [UIColor clearColor];
@@ -148,21 +158,42 @@ forRowAtIndexPath:(NSIndexPath *)indexPath
     [audioPlayer play];
 }
 
+-(void)audioShuffle:(id)sender{
+    UIButton *btn = (UIButton *)sender;
+    if([btn.currentTitle isEqualToString:@"Play"]){
+        NSLog(@"heyheyey");
+        [self playAudio];
+         [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:activeRow inSection:btn.tag]] withRowAnimation:UITableViewRowAnimationNone];
+        
+    }
+    else if([btn.currentTitle isEqualToString:@"Pause"]){
+        [self stopAudio];
+        [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:activeRow inSection:btn.tag]] withRowAnimation:UITableViewRowAnimationNone];
+    }
+    else{
+        NSLog(@"hellohello");
+    }
+
+}
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     if (allowFrameChange && indexPath.row==activeRow) {
         [audioPlayer setCurrentTime:0];
+//        [self stopAudio];
+        allowFrameChange=NO;
+        [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
     }
     else if(allowFrameChange && indexPath.row!=activeRow){
-    allowFrameChange = YES;
+    allowFrameChange = NO;
         
     // Currently reverted to reloadData method since, if we update an individual cell we have to keep track of the previous active cell as well and then update that which requires the previous cell's indexpath and currently we have only row
+        [self stopAudio];
         NSInteger deactivatedRow = activeRow;
         activeRow = indexPath.row;
      [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:deactivatedRow inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationFade];
-    [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    [audioPlayer setCurrentTime:0];
-    [self playAudio];
+//        [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        [audioPlayer setCurrentTime:0];
+//    [self playAudio];
     }
     else{
         allowFrameChange = YES;
@@ -170,9 +201,8 @@ forRowAtIndexPath:(NSIndexPath *)indexPath
         activeRow = indexPath.row;
         [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:deactivatedRow inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationFade];
         allowFrameChange = YES;
+//        [self playAudio];
         [audioTable reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-
-        [self playAudio];
     }
 }
 
